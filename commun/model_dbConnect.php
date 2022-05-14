5549e579194c31076503c7561c4623c9e4d91e6b
Cedric Chantreau <cedricchantreau@gmail.com>
2022-05-14 12:18:19 +0100

    RG2022 settings

--- commun/model_dbConnect.php
+++ commun/model_dbConnect.php
@@ -8,20 +8,21 @@
 	function dbConnect(){
 		try
 		{
-			//----------------------------------
-			//--> Base de donnée de production
-			// $bdd = new PDO('mysql:host=tennisbefubddtbf.mysql.db;dbname=tennisbefubddtbf;charset=utf8', 'tennisbefubddtbf', 'nediamBDDTBF1975', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-			//----------------------------------
-
+			if ($_SERVER['HTTP_HOST'] == 'localhost') {
+				//----------------------------------
+				//--> Base de données pour le test
+				$bdd = new PDO('mysql:host=localhost;dbname=rolandgarros2022;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+				// $bdd = new PDO('mysql:host=localhost;dbname=marseille2020;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+				//----------------------------------
+			} else {
+				//----------------------------------
+				//--> Base de donnée de production
+				$bdd = new PDO('mysql:host=tennisbefubddtbf.mysql.db;dbname=tennisbefubddtbf;charset=utf8', 'tennisbefubddtbf', 'nediamBDDTBF1975', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+				//----------------------------------
+			}
 			//----------------------------------
 			//--> Base de données pour AWS
 			// $bdd = new PDO('mysql:host=localhost;dbname=tennisbetfriends;charset=utf8', 'tbf_username', 'nediamTBF1975', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-			//----------------------------------
-
-			//----------------------------------
-			//--> Base de données pour le test
-			$bdd = new PDO('mysql:host=localhost;dbname=australianopen2022;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-			// $bdd = new PDO('mysql:host=localhost;dbname=marseille2020;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 			//----------------------------------
 
 			return $bdd;
